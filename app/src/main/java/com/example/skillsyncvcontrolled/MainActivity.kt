e8fff1520c12ba38c9d0ce9a119a2412591f4849
Samlinus <samlinus828@gmail.com>
2024-05-04 18:12:35 +0530

    Successful JSON Retrieval

---
+++ app/src/main/java/com/example/skillsyncvcontrolled/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.skillsyncvcontrolled
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okio.IOException
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val url = "http://10.0.2.2:8000/recommendationapi/hello"
+        fetchData(url)
+    }
+
+    private fun fetchData(url: String) {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : okhttp3.Callback {
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
+                e.printStackTrace()
+                // Handle failure
+            }
+
+            override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                    val responseData = response.body?.string() ?: ""
+                    runOnUiThread {
+                        findViewById<TextView>(R.id.textView).text = responseData
+                    }
+                }
+            }
+        })
+
+    }
+}
